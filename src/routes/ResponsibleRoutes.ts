fda7e8e34c9b51055fe514dc2baeeed4a8c3374b
Henrique de Castilhos <henrique@castilhos.dev>
2022-06-30 20:16:02 -0300

    Create controllers and routes files

---
+++ src/routes/ResponsibleRoutes.ts
@@ -0,0 +1,7 @@
+export const ResponsibleRoutes = [
+  // TODO: add get method to all
+  // TODO: add get method to one
+  // TODO: add post method to save
+  // TODO: add put method to update
+  // TODO: add delete method to remove
+];
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,19 @@
 import { AdminRoutes } from "./AdminRoutes";
+import { ResponsibleRoutes } from "./ResponsibleRoutes";
+import { ResumeRoutes } from "./ResumeRoutes";
+import { StudentRoutes } from "./StudentRoutes";
+import { SubjectRoutes } from "./SubjectRoutes";
+import { TeacherRoutes } from "./TeacherRoutes";
+import { TeamRoutes } from "./TeamRoutes";
 import { UserRoutes } from "./UserRoutes";
 
-export const Routes = [...AdminRoutes, ...UserRoutes];
+export const Routes = [
+  ...AdminRoutes,
+  ...ResponsibleRoutes,
+  ...ResumeRoutes,
+  ...StudentRoutes,
+  ...SubjectRoutes,
+  ...TeacherRoutes,
+  ...TeamRoutes,
+  ...UserRoutes,
+];
